af8cd1522885829cca3a1005ba2ccf027d0d9532
GraphicNovel <60416981+graphicnovel@users.noreply.github.com>
2020-10-26 10:51:56 +0900

    common.js 파일 경로 검사 수정

---
+++ common.js
@@ -0,0 +1,32 @@
+$(function () {
+  //start
+
+  //    console.log(location.pathname);
+  //    console.log(location.host);
+
+  $("header").load("/inc_head_foot.html header .h_container", headColor);
+  $("footer").load("/inc_head_foot.html footer .f_container");
+
+  var filename = location.pathname;
+
+  function headColor() {
+    if (filename == "/index.html") {
+      $(".h_container").addClass("dark");
+      $(".contact").addClass("default");
+    } else {
+      $(".h_container").addClass("white");
+      $(".contact").addClass("white");
+    }
+  }
+
+  //  function menuTrigger(){
+  //      // menu-trigger active 클래스 추가
+  //      var menu = document.querySelector('.menu-trigger');
+  //      var nav = document.querySelector('nav');
+
+  //      menu.addEventListener('click',function(){
+  //          menu.classList.toggle('active');
+  //          nav.classList.toggle('active');
+  //      });
+  //  }
+});
