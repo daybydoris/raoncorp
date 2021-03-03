69669fa75ae57f62e79b605f47fe1e9a826acd54
GraphicNovel <onegoseen@gmail.com>
2021-03-03 10:38:13 +0900

    fix: raoninfo history tab toggle event

--- js/raoninfo.js
+++ js/raoninfo.js
@@ -1,9 +1,14 @@
 
 window.addEventListener('DOMContentLoaded', function () {
-    const tabBtn = document.querySelectorAll('.tag_item a');
+    const aBtn = document.querySelectorAll('.tag_item a');
+    const tab = document.querySelector('.tab');
+    const tabBtn = document.querySelectorAll('.tab a');
+    const now = document.querySelector('.now');
+    const then = document.querySelector('.then');
 
-    tabBtn.forEach(function (btn) {
+    aBtn.forEach(function (btn) {
         btn.addEventListener('click', function (e) {
+            //아이디 비교 후 그 요소의 top을 찾아서 그 곳으로 이동
             e.preventDefault();
 
             const where = e.target.getAttribute('href');
@@ -12,11 +17,24 @@ window.addEventListener('DOMContentLoaded', function () {
 
             window.scrollTo({ top: offsetTop, left: 0, behavior: "smooth" });
 
-            console.log(offsetTop);
+        });
+    });
 
-            //아이디 비교 후 그 요소의 top을 찾아서 그 곳으로 이동하기!
+    tab.addEventListener('click', e => {
+        e.preventDefault();
 
+        tabBtn.forEach(btn => {
+            btn.classList.remove('active');
 
+            e.target.classList.add('active');
+
+            if (e.target.classList.contains('tab_now')) {
+                then.classList.remove('active');
+                now.classList.add('active');
+            } else {
+                now.classList.remove('active');
+                then.classList.add('active');
+            }
         });
     });
 });
